c0ebf7e7f3afc717dbbfd10a5adaa3c8087e27ec
«Dmitry_Machnev» <draklion56@gmail.com>
2022-09-15 15:52:15 +0500

    BlockDiagram changed.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество элементов массива: ");
+
+int number = Int32.Parse(Console.ReadLine());
+string[] array = new string[number];
+string[] result = new string[number];
+int count = 0;
+if (number <= 0) Console.WriteLine("Количество элементов массива не может быть меньше нуля.");
+else
+{
+  Console.WriteLine("Введите элементы массива:");
+
+  for (int i = 0; i < array.Length; i++) array[i] = Console.ReadLine();
+
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].ToString().Length <= 3)
+    {
+      result[count] = array[i];
+      count++;
+    }
+  }
+
+
+  if (count == 0) Console.WriteLine("В массиве нет элементов, в которых строка менее трех символов.");
+  else
+  {
+    Console.Write("Результирующий массив: ");
+    for (int i = 0; i < count; i++) Console.Write(result[i] + " ");
+  }
+}
